5b2b9a6998b096e6e2237ca00a89506a188d3fb6
Rishi Raj <rishiraj824@gmail.com>
2020-05-13 06:06:17 -0400

    added report

--- images/views.py
+++ images/views.py
@@ -23,15 +23,17 @@ def upload(request):
             image = loadImage(os.path.join(BASE_DIR, 'media/images/') + name, 1)
             wiener = WienerFilter(image, (11, 11))
             output = wiener.estimateOutputColorised()
+            sharpen_kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+            sharpen = cv.filter2D(output, -1, sharpen_kernel)
         else:
             image = loadImage(os.path.join(BASE_DIR, 'media/images/') + name, 0)
             wiener = WienerFilter(image, (5, 5))
             output = wiener.estimateOutput()
+            sharpen_kernel = np.array([[0,-1,0], [-1,7,-1], [0,-1,0]])
+            sharpen = cv.filter2D(output, -1, sharpen_kernel)
 
-        # sharpen_kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
-        # sharpen = cv.filter2D(output, -1, sharpen_kernel)
-        # print(sharpen)
-        cv.imwrite(os.path.join(BASE_DIR, 'media/images/') + 'processed_' + name, output)
+
+        cv.imwrite(os.path.join(BASE_DIR, 'media/images/') + 'processed_' + name, sharpen)
 
         return JsonResponse(
             {"message": "Image Succesfully Processed", "image": 'http://127.0.0.1:8000/media/images/' + 'processed_' + name},
